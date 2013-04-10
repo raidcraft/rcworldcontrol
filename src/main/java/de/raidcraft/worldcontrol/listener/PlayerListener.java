1b2f35a3068674d3c45c9d983f963c2c8c2391e3
Philip Urban <urban.philip@googlemail.com>
2013-04-10 22:13:07 +0200

    fixed bucket fill event

--- src/main/java/de/raidcraft/worldcontrol/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/worldcontrol/listener/PlayerListener.java
@@ -5,12 +5,10 @@ import de.raidcraft.worldcontrol.WorldControlPlugin;
 import de.raidcraft.worldcontrol.util.WorldGuardManager;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 
 /**
  * Author: Philip
@@ -40,21 +38,21 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    public void onPlayerFillBucket(PlayerBucketFillEvent event) {
 
-        if(event.getAction() != Action.LEFT_CLICK_BLOCK) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+            return;
+
+        //check world
+        if (!event.getPlayer().getLocation().getWorld().getName().equalsIgnoreCase(RaidCraft.getComponent(WorldControlPlugin.class).config.world))
+            return;
+
+        //check if location is region
+        if(WorldGuardManager.INST.isInUnknownRegion(event.getPlayer().getLocation())) {
             return;
         }
 
-        if(event.getPlayer().getItemInHand().getType() != Material.BUCKET) {
-            return;
-        }
-
-        if(event.getClickedBlock() == null || (event.getClickedBlock().getType() != Material.WATER && event.getClickedBlock().getType() != Material.LAVA)) {
-            return;
-        }
-
-        event.getPlayer().sendMessage(ChatColor.RED + "Du kannst hier dein Eimer nicht füllen!");
         event.setCancelled(true);
+        event.getPlayer().sendMessage(ChatColor.RED + "Du kannst hier dein Eimer nicht füllen!");
     }
 }
