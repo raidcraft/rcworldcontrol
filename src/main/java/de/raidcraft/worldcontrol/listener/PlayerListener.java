be6410c7f71f258f4505b44cf9607b4e2ffe50b1
Philip Urban <urban.philip@googlemail.com>
2013-04-22 17:20:18 +0200

    changed bucket behaviour

--- src/main/java/de/raidcraft/worldcontrol/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/worldcontrol/listener/PlayerListener.java
@@ -5,10 +5,13 @@ import de.raidcraft.worldcontrol.WorldControlPlugin;
 import de.raidcraft.worldcontrol.util.WorldGuardManager;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Author: Philip
@@ -20,21 +23,30 @@ public class PlayerListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerEmptyBucket(PlayerBucketEmptyEvent event) {
 
-        if (event.getPlayer().getGameMode() == GameMode.CREATIVE)
+        Player player = event.getPlayer();
+
+        if (player.getGameMode() == GameMode.CREATIVE)
             return;
 
         //check world
-        if (!event.getPlayer().getLocation().getWorld().getName().equalsIgnoreCase(RaidCraft.getComponent(WorldControlPlugin.class).config.world))
+        if (!player.getLocation().getWorld().getName().equalsIgnoreCase(RaidCraft.getComponent(WorldControlPlugin.class).config.world))
             return;
 
         //check if location is region
-        if(WorldGuardManager.INST.isInUnknownRegion(event.getPlayer().getLocation())) {
+        if(WorldGuardManager.INST.isInUnknownRegion(player.getLocation())) {
             return;
         }
 
         //prevent lava and water placement
         event.setCancelled(true);
-        event.getPlayer().sendMessage(ChatColor.RED + "Du kannst hier nichts ausschütten!");
+
+        if(player.getFireTicks() > 0) {
+            player.setFireTicks(0);
+            player.setItemInHand(new ItemStack(Material.BUCKET));
+            player.sendMessage(ChatColor.GREEN + "Du hast dich gelöscht!");
+            return;
+        }
+        player.sendMessage(ChatColor.RED + "Du kannst hier nichts ausschütten!");
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -53,6 +65,12 @@ public class PlayerListener implements Listener {
         }
 
         event.setCancelled(true);
-        event.getPlayer().sendMessage(ChatColor.RED + "Du kannst hier deinen Eimer nicht füllen!");
+        if(event.getBlockClicked().getType() == Material.STATIONARY_WATER || event.getBlockClicked().getType() == Material.WATER) {
+            event.getPlayer().setItemInHand(new ItemStack(Material.WATER_BUCKET));
+        }
+        if(event.getBlockClicked().getType() == Material.STATIONARY_LAVA || event.getBlockClicked().getType() == Material.LAVA) {
+            event.getPlayer().setItemInHand(new ItemStack(Material.LAVA_BUCKET));
+        }
+        event.getPlayer().updateInventory();
     }
 }
